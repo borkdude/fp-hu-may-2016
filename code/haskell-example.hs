c5c6fd306e1b8d1f2fb80acf2f229409308fc24f
Michiel Borkent <michielborkent@gmail.com>
2016-05-08 16:19:26 +0200

    First commit

---
+++ code/haskell-example.hs
@@ -0,0 +1,21 @@
+import Data.List
+       
+data Person = Person { firstname :: String
+                     , lastname :: String } deriving (Show)
+
+hickey = Person { firstname = "Rich", lastname = "Hickey" }
+odersky = Person { firstname = "Martin", lastname = "Odersky" }
+wadler = Person { firstname = "Philip", lastname = "Wadler" }
+
+persons = [hickey,odersky,wadler]
+
+mapped = map firstname persons
+filtered = filter (\name -> (length name) > 4) mapped
+reduced = foldl (\acc name -> acc + length name) 0 filtered 
+
+countTotalLengthOffirstNamesLongerThanFourCharacters persons =
+  let firstnames = map firstname persons
+      longerThan4 = filter (\name -> (length name) > 4) mapped
+      totalLength = foldl (\acc name -> acc + length name) 0 filtered
+  in totalLength
+
