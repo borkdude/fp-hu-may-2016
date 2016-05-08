9a256e541ede4bbf873828240965ca980a24644d
Michiel Borkent <michielborkent@gmail.com>
2016-05-08 21:19:58 +0200

    Upgrade

--- code/animals-crud/project.clj
+++ code/animals-crud/project.clj
@@ -1,33 +1,34 @@
 (defproject animals-crud "0.1.0-SNAPSHOT"
   :description "FIXME: write this!"
   :url "http://example.com/FIXME"
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-3211"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]                 
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.8.51"]
+                 [org.clojure/core.async "0.2.374"]
                  [ring-server "0.4.0"]
-                 [org.webjars/bootstrap "3.2.0"]
-                 [cljs-http "0.1.30"]
-                 [compojure "1.3.4"]
-                 [liberator "0.13"]
-                 [fogus/ring-edn "0.2.0"]
+                 [compojure "1.5.0"]
+                 [org.webjars/bootstrap "3.3.6"]
+                 [cljs-http "0.1.40"]
+                 [liberator "0.14.1"]
+                 [fogus/ring-edn "0.3.0"]
                  [clj-json "0.5.3"]
-                 [reagent "0.5.0"]
-                 [prismatic/schema "0.4.3"]
-                 [org.clojure/java.jdbc "0.4.2"]
-                 [com.h2database/h2 "1.4.189"]]
+                 [hiccup "1.0.5"]
+                 [reagent "0.5.1"]
+                 [prismatic/schema "1.1.1"]
+                 [org.clojure/java.jdbc "0.5.8"]
+                 [com.h2database/h2 "1.4.191"]]
 
-  :plugins [[lein-cljsbuild "1.0.5"]]
+  :plugins [[lein-cljsbuild "1.1.3"]]
 
   :clean-targets ^{:protect false} [:target-path :compile-path "resources/public/out"]
 
   :source-paths ["src"]
 
-  :profiles {:dev {:plugins [[lein-figwheel "0.3.1"]]
+  :profiles {:dev {:plugins [[lein-figwheel "0.5.3-1"]]
                    :figwheel {:http-server-root "public"
                               :port 3449}}}
 
   :cljsbuild {:builds [{:id "reagent"
-                        :source-paths ["src-cljs"]
+                        :source-paths ["src-cljs/"]
                         :figwheel true
                         :compiler {:output-to "resources/public/crud.js"
                                    :output-dir "resources/public/out"
